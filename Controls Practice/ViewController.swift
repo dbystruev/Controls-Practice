afc2296e49bb399bb52a945948a817374aa99c0d
Denis Bystruev <dbystruev@me.com>
2020-08-05 16:21:28 +0300

    Initial Commit

---
+++ Controls Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Controls Practice
+//
+//  Created by Denis Bystruev on 05.08.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
